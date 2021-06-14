01df979503cd655db62bcd956ae68ef525f3b0c4
Emiko Esther S <emiko.esther.self@gmail.com>
2021-06-14 15:41:43 -0700

    added login, sign up, and user authorization using Auth0

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// dev - p72vwee0.us.auth0.com;
+// tmhKrrxwcHGobYJqh6OwZoN2ala8YZ8P
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-p72vwee0.us.auth0.com'
+      clientId='tmhKrrxwcHGobYJqh6OwZoN2ala8YZ8P'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
